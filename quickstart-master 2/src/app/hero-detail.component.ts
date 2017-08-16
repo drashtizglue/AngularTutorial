23f9c9caaa7cd6758c6dc1f882a5025e81731aad
drashtizglue <31018185+drashtizglue@users.noreply.github.com>
2017-08-16 16:41:06 -0700

    Use Switchmap

--- quickstart-master 2/src/app/hero-detail.component.ts
+++ quickstart-master 2/src/app/hero-detail.component.ts
@@ -1,4 +1,10 @@
-import { Component, Input } from '@angular/core';
+// Keep the Input import for now, you'll remove it later:
+import { Component, Input, OnInit } from '@angular/core';
+import { ActivatedRoute, ParamMap } from '@angular/router';
+import { Location }                 from '@angular/common';
+import 'rxjs/add/operator/switchMap';
+
+import { HeroService } from './hero.service';
 import { Hero } from './hero';
 
 @Component({
@@ -16,6 +22,22 @@ import { Hero } from './hero';
   `
 })
 
-export class HeroDetailComponent {
-	@Input() hero: Hero;
+export class HeroDetailComponent implements OnInit {
+
+
+  constructor(
+    private heroService: HeroService,
+    private route: ActivatedRoute,
+    private location: Location
+  ) {}
+
+  ngOnInit(): void {
+    this.route.paramMap
+    .switchMap((params: ParamMap) => this.heroService.getHero(+params.get('id')))
+    .subscribe(hero => this.hero = hero);
+  }
+
+  goBack(): void {
+    this.location.back();
+  }
 }
