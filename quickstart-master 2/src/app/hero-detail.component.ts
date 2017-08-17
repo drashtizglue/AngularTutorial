523541470bcd81763fbb934a4a44f702f1788c49
drashtizglue <31018185+drashtizglue@users.noreply.github.com>
2017-08-17 10:56:34 -0700

    Find a way back

--- quickstart-master 2/src/app/hero-detail.component.ts
+++ quickstart-master 2/src/app/hero-detail.component.ts
@@ -9,17 +9,7 @@ import { Hero } from './hero';
 
 @Component({
   selector: 'hero-detail',
-  template: `
-    <div *ngIf="hero">
-      <h2>{{hero.name}} details!</h2>
-      <div><label>id: </label>{{hero.id}}</div>
-      <div>
-        <label>name: </label>
-        <input [(ngModel)]="hero.name" placeholder="name"/>
-        <hero-detail [hero]="selectedHero"></hero-detail>
-      </div>
-    </div>
-  `
+  templateUrl: './hero-detail.component.html',
 })
 
 export class HeroDetailComponent implements OnInit {
